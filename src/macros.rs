abb48d9a8d5bd30779882b3b53be7f3d80702696
Josh Stoik <josh@blobfolio.com>
2022-09-09 00:50:02 -0700

    revert: use eprint! instead of io::Write to avoid possible double trait import

--- src/macros.rs
+++ src/macros.rs
@@ -75,31 +75,20 @@ macro_rules! benches {
 	($($benches:expr),+ $(,)?) => {
 		/// # Benchmarks!
 		fn main() {
-			use ::std::io::Write;
-
-			let writer = ::std::io::stderr();
-			let mut handle = writer.lock();
-
 			// Announce that we've started.
-			let _res = handle.write_all(b"\x1b[1;38;5;199mStarting:\x1b[0m Running benchmark(s). Stand by!\n\n")
-				.and_then(|_| handle.flush());
+			::std::eprint!("\x1b[1;38;5;199mStarting:\x1b[0m Running benchmark(s). Stand by!\n\n");
 
 			// Run the benches.
 			let mut benches = $crate::Benches::default();
 			$(
 				// Print a dot to show some progress.
-				let _res = handle.write_all(b"\x1b[1;34m\xe2\x80\xa2\x1b[0m")
-					.and_then(|_| handle.flush());
+				::std::eprint!("\x1b[1;34m•\x1b[0m");
 
 				benches.push($benches);
 			)+
 
-			// Print a line break.
-			let _res = handle.write_all(b"\n").and_then(|_| handle.flush());
-
-			// Cleanup.
-			drop(handle);
-			drop(writer);
+			// Give some space.
+			::std::eprint!("\n\n");
 
 			// Finish!
 			benches.finish();
